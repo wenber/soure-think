667b9e4016690a8032217ceca513b21893722c82
liuming <liuming@kuaishou.com>
2016-12-13 14:06:58 +0800

    rm ant

--- ant-design/index.js
+++
@@ -1,9 +0,0 @@
-// this file is not used if use https://github.com/ant-design/babel-plugin-antd
-
-import warn from 'rc-util/lib/warn';
-import * as antd from './components';
-
-warn('You are using prebuilt antd, ' +
-     'please use https://github.com/ant-design/babel-plugin-antd to reduce app bundle size.');
-
-export default antd;
